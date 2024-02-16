7a40a3520271444f4a5fd9d5690d89c4874b3151
Kshitij Chauhan <kshitij.cx@gmail.com>
2024-02-16 18:06:46 +0530

    initial commit

---
+++ src/components/Button.jsx
@@ -0,0 +1,9 @@
+const Button = ({label, iconURL}) => {
+  return (
+    <button className="flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red rounded-full text-white border-coral-red">
+        {label}
+        {iconURL && <img className="ml-2 rounded-full w-5 h-5" src={iconURL} alt="" />}
+    </button>
+  )
+}
+export default Button
